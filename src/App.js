ebe97ad9fdf9806b1eea64b1908c60140d1ca39a
kokivasileski <thekoki001@gmail.com>
2022-03-18 12:25:26 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.scss";
 
 function App() {
   return (
-    <h1>hello</h1>
-    <h2>hello again</h2>
+      <>
+        <h1>hello</h1>
+        <h2>hello again</h2>
+      </>
   );
 }
 
