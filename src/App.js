723ee90e0caf8077d21342a443b42e1c71ec0f0d
mackenzie <macmcclemont@hotmail.com>
2022-03-19 15:04:59 -0400

    deleted a few comments

--- src/App.js
+++ src/App.js
@@ -6,86 +6,9 @@ import "./App.scss";
 import MapForm from './Components/MapForm';
 import PodcastGenreForm from './Components/PodcastGenreForm';
 
-
-
-// Api URL: "http://www.mapquestapi.com/directions/v2/route"
-// Api key: pXPeEb8fKG1bWJTjmqYRZoLhF0sGhYUW
 function App() {
 
-//   const [ givenAddress, setGivenAddress ] = useState([]);
 
-
-//   useEffect(function() {
-    
-//     axios({
-//       url: "http://www.mapquestapi.com/directions/v2/route",
-//       params: {
-//         key: "pXPeEb8fKG1bWJTjmqYRZoLhF0sGhYUW",
-//         from: "Toronto, CA",
-//         to: "Hamilton, CA",
-//         unit: 'k',
-//         routeType: "bicycle",
-//       }
-//     }).then(function(apiData){
-//         // console.log(apiData)
-//     })
-//     // axios({
-//     //   url: "https://www.mapquestapi.com/staticmap/v5/map",
-//     //   params: {
-//     //     key: "pXPeEb8fKG1bWJTjmqYRZoLhF0sGhYUW",
-//     //     format: "png",
-//     //     // size: "500,200@2x",
-//     //     // start: "Toronto, CA",
-//     //     // end: "Hamilton, CA",
-//     //     session: "6234e2b1-005d-6750-02b4-34e5-12e03e48224f" 
-//     //   }
-//     // }).then(function (apiData) {
-//     //   console.log(apiData.request.responseURL)
-//     // })
-    
-//     axios({
-//       url: 'https://listen-api.listennotes.com/api/v2/search',
-//       headers: { "X-ListenAPI-Key": "0be4947c18024c2d8a5bb0dcb11eb2ac" },
-//       params: {
-//         q: 'star wars'
-
-//       }
-
-//     }).then( (response) => {
-//       // console.log(response.data.results)
-//     })
-//   }, [])
-// // 
-//   const [input, setInput] = useState("");
-
-//   useEffect(function () {
-//     if (input.length > 1) {
-//       axios({
-//         url: 'http://www.mapquestapi.com/search/v3/prediction',
-//         params: {
-//           key: 'pXPeEb8fKG1bWJTjmqYRZoLhF0sGhYUW',
-//           q: `${input}`,
-//           collection: 'address',
-//           limit: 5,
-//           countryCode: 'CA'
-
-  
-//         }
-  
-//       }).then((response) => {
-//         // console.log(response.data.results)
-//         setGivenAddress(response.data.results)
-//       })
-
-      
-//     }
-//   }, [input])
-
-  
-//   console.log(input)
-//   const handleInput = (e) => {
-//       setInput(e.target.value)
-//   }
 
   return (
       <>
