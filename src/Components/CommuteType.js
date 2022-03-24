d492db45ad154099c191a359993d0d4766e12fea
william <williamw8828@gmail.com>
2022-03-24 03:14:27 -0400

    added styling

--- src/Components/CommuteType.js
+++ src/Components/CommuteType.js
@@ -1,4 +1,7 @@
 import Swal from 'sweetalert2'
+import { AiFillCar } from 'react-icons/ai';
+import { MdDirectionsBike } from 'react-icons/md'
+import { FaWalking } from 'react-icons/fa'
 
 const CommuteType = (props) => {
 
@@ -10,13 +13,14 @@ const CommuteType = (props) => {
 
   return (
     <div className="commuteType">
-      <form action="" id="types" style={{display: "flex"}}>
+      <h2>How Will You Be Travelling?</h2>
+      <form action="" id="types" className='commuteIconsForm'>
         <input type="radio" id="walk" name="types" className="sr-only"
         onClick={() => {handleChange(props.walkTime.time, props.walkTime.sessionId, props.walkTime.options.routeType)}}
         />
-        <label htmlFor="walk">
-          <div style={{height: "100px", width: "100px", border: "2px solid red"}}>
-            <p>Walk</p>
+        <label htmlFor="walk" aria-label="walking time" className='travelIconsLabel'>
+          <div className='individualIcons'>
+            <FaWalking />
             {
               props.walkTime.formattedTime === undefined && props.bikeTime.formattedTime !== undefined
               ? <p>DON'T WALK TOO FAR</p>
@@ -27,18 +31,18 @@ const CommuteType = (props) => {
         <input type="radio" id="bike" name="types" className="sr-only"
           onClick={() => { handleChange(props.bikeTime.time, props.bikeTime.sessionId, props.bikeTime.options.routeType)}}
         />
-        <label htmlFor="bike">
-          <div style={{ height: "100px", width: "100px", border: "2px solid red" }}>
-            <p>Bike</p>
+        <label htmlFor="bike" aria-label="biking time" className='travelIconsLabel'>
+          <div className='individualIcons'>
+            <MdDirectionsBike />
             <p>{props.bikeTime.formattedTime}</p>
           </div>
         </label>
         <input type="radio" id="drive" name="types" className="sr-only"
           onClick={() => { handleChange(props.driveTime.time, props.driveTime.sessionId, props.driveTime.options.routeType)}}
         />
-        <label htmlFor="drive">
-          <div style={{ height: "100px", width: "100px", border: "2px solid red" }}>
-            <p>Drive</p>
+        <label htmlFor="drive" aria-label="driving time" className='travelIconsLabel'>
+          <div className='individualIcons'>
+            <AiFillCar />
             <p>{props.driveTime.formattedTime}</p>
           </div>
         </label>
